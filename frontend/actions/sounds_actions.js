c6e05fc1a4f355be206aa37dc282ff025c28d05c
“delrod” <thedavidelrod@gmail.com>
2020-12-12 15:21:17 -0900

    finished actions

---
+++ frontend/actions/sounds_actions.js
@@ -0,0 +1,67 @@
+import * as SoundAPI from "../util/sounds_util";
+
+export const RECEIVE_SOUNDS = `RECEIVE_SOUNDS`;
+export const RECEIVE_SOUND = `RECEIVE_SOUND`;
+export const REMOVE_SOUND = `REMOVE_SOUND`;
+export const RECEIVE_SOUND_ERRORS = `RECEIVE_SOUND_ERRORS`;
+export const REQUEST_SOUND_FETCH = `REQUEST_SOUND_FETCH`;
+
+export const receiveSounds = (sounds) => ({
+  type: RECEIVE_SOUNDS,
+  sounds,
+});
+
+export const receiveSound = (sound) => ({
+  type: RECEIVE_SOUND,
+  sound,
+});
+
+export const removeSound = (soundId) => ({
+  type: REMOVE_SOUND,
+  soundId,
+});
+
+export const receiveSoundErrors = (errors) => ({
+  type: RECEIVE_SOUND_ERRORS,
+  errors,
+});
+
+export const requestSoundFetch = () => ({
+  type: REQUEST_SOUND_FETCH,
+});
+
+export const fetchSounds = () => (dispatch) => {
+  return SoundAPI.fetchSounds().then(
+    (sounds) => dispatch(receiveSounds(sounds)),
+    (err) => dispatch(receiveSoundErrors(err.responseJSON))
+  );
+};
+
+export const fetchSound = (id) => (dispatch) => {
+  dispatch(requestSoundFetch());
+  return SoundAPI.fetchSound(id).then(
+    (sound) => dispatch(receiveSound(sound)),
+    (err) => dispatch(receiveTrackErrors(err.responseJSON))
+  );
+};
+
+export const createSound = (sound) => (dispatch) => {
+  return SoundAPI.createSound(sound).then(
+    (sound) => dispatch(receiveSound(sound)),
+    (err) => dispatch(err.responseJSON)
+  );
+};
+
+export const updateSound = (sound, id) => (dispatch) => {
+  return SoundAPI.updateSound(sound, id).then(
+    (sound) => dispatch(receiveSound(sound)),
+    (err) => dispatch(err.responseJSON)
+  );
+};
+
+export const deleteSound = (soundId) => (dispatch) => {
+  return SoundAPI.deleteSound(soundId).then(
+    (sound) => dispatch(removeSound(sound)),
+    (err) => dispatch(err.responseJSON)
+  );
+};
---
+++ frontend/util/sounds_util.js
@@ -0,0 +1,43 @@
+export const fetchSounds = () => (
+  $.ajax({
+    method: "GET",
+    url: `api/sounds`,
+  })
+);
+
+export const fetchSound = (id) => (
+  $.ajax({
+    method: `GET`,
+    url: `api/sounds/${id}`,
+  })
+);
+
+export const createSound = (sound) => (
+  $.ajax({
+    method: `POST`,
+    url: `api/sounds`,
+    processData: false,
+    contentType: false,
+    dataType: `json`,
+    data: { sound },
+  })
+);
+
+export const updateSound = (sound, id) =>
+  $.ajax({
+    method: `PATCH`,
+    url: `api/sounds/${id}`,
+    processData: false,
+    contentType: false,
+    dataType: `json`,
+    data: {sound}
+  });
+
+export const deleteSound = (id) => (
+    $.ajax({
+        method: "DELETE",
+        url: `api/sounds/${id}`
+    })
+);
+
+
