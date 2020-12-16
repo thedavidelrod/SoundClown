70d4eb3c393463bc5f5c27c2005578515cb8a85b
“delrod” <thedavidelrod@gmail.com>
2020-12-16 10:33:20 -0900

    deleted old player

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import  playerReducer  from "./player_reducer";
 import modalReducer from "./modal_reducer";
 
 
 const uiReducer = combineReducers({
   modal: modalReducer,
-  player: playerReducer
 });
 
 export default uiReducer;
