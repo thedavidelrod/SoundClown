2d0bbcb2e7a0fb30345752c20e5a9387028d1aa6
“delrod” <thedavidelrod@gmail.com>
2021-01-02 17:59:31 -0900

    add comment actions to show container

--- frontend/components/sounds/sound_show_container.jsx
+++ frontend/components/sounds/sound_show_container.jsx
@@ -2,6 +2,12 @@ import { connect } from "react-redux";
 import SoundShow from './sound_show'
 import {fetchSounds, fetchSound, deleteSound, updateSound} from '../../actions/sounds_actions'
 import {withRouter} from 'react-router-dom'
+import {
+  fetchComments,
+  createComment,
+  deleteComment,
+} from "../../actions/comment_actions";
+
 const mSTP = (state, ownProps) => ({
   sound: state.entities.sounds[ownProps.match.params.id],
   errors: state.errors.sounds,
@@ -13,6 +19,9 @@ const mDTP = (dispatch) => ({
   fetchSound: (id) => dispatch(fetchSound(id)),
   deleteSound: (id) => dispatch(deleteSound(id)),
   updateSound: (sound, id) => dispatch(updateSound(sound, id)),
+  fetchComments: () => dispatch(fetchComments()),
+  createComment: (comment) => dispatch(createComment(comment)),
+  deleteComment: (id) => dispatch(deleteComment(id)),
 });
 
 export default withRouter(connect(mSTP, mDTP)(SoundShow));
