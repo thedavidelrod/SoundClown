8205d0f35613b8b37b1018f77a27dc03f9e1700f
“delrod” <thedavidelrod@gmail.com>
2021-01-02 16:46:03 -0900

    comment reducer

---
+++ frontend/reducers/comment_reducer.js
@@ -0,0 +1,26 @@
+import {
+  RECEIVE_COMMENTS,
+  RECEIVE_COMMENT,
+  DELETE_COMMENT,
+} from "../actions/comment_actions";
+
+const commentsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+
+  switch (action.type) {
+    case RECEIVE_COMMENTS:
+      return action.comments;
+    case RECEIVE_COMMENT:
+      return Object.assign({}, oldState, {
+        [action.comment.id]: action.comment,
+      });
+    case DELETE_COMMENT:
+      let nextState = Object.assign({}, oldState);
+      delete nextState[action.commentId];
+      return nextState;
+    default:
+      return oldState;
+  }
+};
+
+export default commentsReducer;
